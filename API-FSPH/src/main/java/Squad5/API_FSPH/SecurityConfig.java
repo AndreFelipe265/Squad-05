f960afe9c1803a97db7d7633d34c34e2b0e04f2c
André Felipe <160677791+AndreFelipe265@users.noreply.github.com>
2025-05-07 20:13:39 -0300

    h2

--- API-FSPH/src/main/java/Squad5/API_FSPH/SecurityConfig.java
+++ API-FSPH/src/main/java/Squad5/API_FSPH/SecurityConfig.java
@@ -1,8 +1,6 @@
 package Squad5.API_FSPH;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,14 +9,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable()) // Desabilitar CSRF enquanto você não usa token ou login por formulário
+        http
                 .authorizeHttpRequests(auth -> auth
-                        // Liberando as rotas públicas (incluindo a base /v1/Amostra)
-                        .requestMatchers("/v1/Amostra/**").permitAll()  // Libera acesso a todas as rotas dentro de /v1/Amostra
-                        .anyRequest().authenticated()  // Requer autenticação para outras rotas
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults()) // Autenticação básica (pode ser substituída por JWT no futuro)
-                .build();
+                .csrf().disable() // Desabilita CSRF (apenas se necessário)
+                .formLogin().disable(); // Desabilita o formulário de login
+        return http.build();
     }
 }
