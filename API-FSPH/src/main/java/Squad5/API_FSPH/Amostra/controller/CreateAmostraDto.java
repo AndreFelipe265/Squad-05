a46070d6deb76ed8335a587fc0c4088db5c2c0f4
André Felipe <160677791+AndreFelipe265@users.noreply.github.com>
2025-05-15 00:34:57 -0300

    Ajustes Amostra

---
+++ API-FSPH/src/main/java/Squad5/API_FSPH/Amostra/controller/CreateAmostraDto.java
@@ -0,0 +1,19 @@
+package Squad5.API_FSPH.Amostra.controller;
+
+import Squad5.API_FSPH.Amostra.entity.Tipo;
+import jakarta.validation.constraints.*;
+
+import java.util.UUID;
+
+// DTO usado para criar uma nova amostra.
+
+public record CreateAmostraDto(
+        @NotNull Tipo tipo,                   // Tipo da amostra (ex: MOSQUITO)
+        @NotBlank String localCaptura,        // Local de captura (ex: área urbana, rural, matagal)
+        @NotBlank String enderecoCaptura,     // Endereço da coleta da amostra
+        @NotBlank String dataCaptura,         // Data da coleta no formato ISO (yyyy-MM-dd)
+        String insetoLarva,                   // Nome do inseto ou larva, se aplicável
+        @NotNull UUID municipioId,            // ID do município
+        @NotBlank String municipioNome,       // Nome do município
+        @NotBlank String protocoloLote        // Protocolo do lote a que pertence
+) {}
